e7bd81606687663ea6dadba14a337bccd65c4418
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-08-14 03:38:21 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ezlog",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     description="A simple logging utility.",
     long_description=open("README.md").read(),
